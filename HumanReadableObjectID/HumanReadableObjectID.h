f6a015af17585a84ae0ace546ea5a5267b650ea5
Jim Coven <jattcode@gmail.com>
2015-07-16 11:36:23 +0800

    init empty project

---
+++ HumanReadableObjectID/HumanReadableObjectID.h
@@ -0,0 +1,19 @@
+//
+//  HumanReadableObjectID.h
+//  HumanReadableObjectID
+//
+//  Created by Javan Tan on 16/7/15.
+//  Copyright (c) 2015 MikiMedia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HumanReadableObjectID.
+FOUNDATION_EXPORT double HumanReadableObjectIDVersionNumber;
+
+//! Project version string for HumanReadableObjectID.
+FOUNDATION_EXPORT const unsigned char HumanReadableObjectIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HumanReadableObjectID/PublicHeader.h>
+
+
