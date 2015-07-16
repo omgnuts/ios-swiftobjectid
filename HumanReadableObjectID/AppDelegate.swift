47d54eced21b9404c69690ae1e535683698e2d9a
Jim Coven <jattcode@gmail.com>
2015-07-16 11:49:04 +0800

    changed to swift based project template

---
+++ HumanReadableObjectID/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HumanReadableObjectID
+//
+//  Created by Javan Tan on 16/7/15.
+//  Copyright (c) 2015 MikiMedia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HumanReadableObjectID/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HumanReadableObjectID
+//
+//  Created by Javan Tan on 16/7/15.
+//  Copyright (c) 2015 MikiMedia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
